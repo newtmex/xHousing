832b0dd8ef78f9005d4ae21428cc13191a8d396d
newtmex <brightomex@gmail.com>
2024-06-07 12:03:20 +0100

    final testnet build

--- x-housing-ui/src/config/config.testnet.ts
+++ x-housing-ui/src/config/config.testnet.ts
@@ -7,10 +7,10 @@ export const chainID = 'T';
 export const environment = EnvironmentsEnum.testnet;
 
 export const coinbaseAddress =
-  'erd1qqqqqqqqqqqqqpgqzpfdhdtq4p768ky8l47xnfrea2g4h9ck0fusadz42w';
+  'erd1qqqqqqqqqqqqqpgqucyqvqgmyf0avp76evd7aqrxskvcj35e0fusnf5gfq';
 export const xHousingAddress =
-  'erd1qqqqqqqqqqqqqpgqv6fc7m9fymejw6ckykxhlcpw4w5txqqe0fus6sl8ye';
+  'erd1qqqqqqqqqqqqqpgqs22spcvt0c4rl8zmfyufwxuqu2xneze40fus6t92fu';
 export const xProjectFundingAddress =
-  'erd1qqqqqqqqqqqqqpgqtrp9ultkezqhc2fmy4wjnsy3pyvks2v90fusytuza0';
+  'erd1qqqqqqqqqqqqqpgql5664ympcv9cfg7semytlsevw4jcu0gf0fusdshzex';
 export const apiAddress = 'https://testnet-api.multiversx.com';
 export const proxyAddress = 'https://testnet-gateway.multiversx.com';
