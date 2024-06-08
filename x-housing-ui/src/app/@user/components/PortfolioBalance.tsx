0c15ab2a8acf07b0b4a75e4e71e8cdd06014060a
newtmex <brightomex@gmail.com>
2024-06-08 16:05:38 +0100

    Prettier table display

--- x-housing-ui/src/app/@user/components/PortfolioBalance.tsx
+++ x-housing-ui/src/app/@user/components/PortfolioBalance.tsx
@@ -1,11 +1,72 @@
 import { useXProjects } from '@/contracts/xProject/hooks';
-import { useAccountTokens, useTokenPrices } from '@/hooks';
+import {
+  useAccountTokens,
+  useTokenPrices,
+  useWindowWidthChange
+} from '@/hooks';
 import { prettyFormatAmount } from '@/utils';
 import { RoutePath } from '@/utils/routes';
 import { NonFungibleTokenOfAccountOnNetwork } from '@multiversx/sdk-network-providers/out';
 import BigNumber from 'bignumber.js';
 import Link from 'next/link';
-import { useMemo } from 'react';
+import { ReactNode, useEffect, useMemo, useState } from 'react';
+
+interface TableProps<TableData> {
+  data: TableData[];
+  renderTableData: (data: TableData, index: number) => ReactNode;
+}
+
+function TabTable<T = any>({ data, renderTableData }: TableProps<T>) {
+  const [rows, setRows] = useState<(typeof data)[]>([]);
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useWindowWidthChange(() => {
+    setWindowWidth(window.innerWidth);
+  });
+
+  // Do data grouping
+  useEffect(() => {
+    let rowsCount =
+      data.length > 3
+        ? windowWidth <= 768 || data.length >= 7
+          ? Math.ceil(data.length / 2)
+          : 2
+        : 1;
+
+    const rows: (typeof data)[] = [];
+    let rowDataCount = Math.floor(data.length / rowsCount);
+    rowDataCount <= 0 && (rowDataCount = 1);
+
+    let remainder = data.length < rowsCount ? 0 : data.length % rowsCount;
+    let sliceLimit = remainder > 0 ? rowDataCount + 1 : rowDataCount;
+    for (let i = 0; i < data.length; i += sliceLimit) {
+      remainder > 0 ? (remainder -= 1) : (sliceLimit = rowDataCount);
+
+      rows.push(data.slice(i, i + sliceLimit));
+    }
+
+    setRows(rows);
+  }, [data, windowWidth]);
+
+  if (rows.length <= 0) {
+    return null;
+  }
+
+  return (
+    <table
+      style={{ width: 0 }}
+      className='table table-lightborder table-bordered table-v-compact mb-0'
+    >
+      <tbody>
+        {rows.map((row, index) => (
+          <tr key={'tr' + index}>
+            {row.map((data, index) => renderTableData(data, index))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 export default function PortfolioBalance() {
   const prices = useTokenPrices();
@@ -48,20 +109,17 @@ export default function PortfolioBalance() {
           </div>
         </div>
         <div className='balance-table pl-sm-2'>
-          <table className='table table-lightborder table-bordered table-v-compact mb-0'>
-            <tbody>
-              <tr>
-                {xProjectsTokenDetail.map((token) => (
-                  <td key={token.collection}>
-                    <strong>${prices[token.collection].toFixed(2)}</strong>
-                    <div className='balance-label smaller lighter text-nowrap'>
-                      {token.name} {token.collection}
-                    </div>
-                  </td>
-                ))}
-              </tr>
-            </tbody>
-          </table>
+          <TabTable
+            data={xProjectsTokenDetail}
+            renderTableData={(token) => (
+              <td key={token.collection}>
+                <strong>${prices[token.collection].toFixed(2)}</strong>
+                <div className='balance-label smaller lighter text-nowrap'>
+                  {token.name} {token.collection}
+                </div>
+              </td>
+            )}
+          />
         </div>
       </div>
       <div className='element-wrapper pb-4 mb-4 border-bottom'>
